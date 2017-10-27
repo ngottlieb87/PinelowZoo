3a381fbe99ddb1975b81d54ff7b2c3cc2ab4e9e9
ngottlieb87 <nogpdx@gmail.com>
2017-10-27 09:02:25 -0700

    add animal class and objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,17 @@ import { Animal } from './animal.model'
   selector: 'app-root',
   template: `
   <div class= 'container'>
+    <h1>{{currentFocus}}</h1>
   </div>
 
   `
 })
   export class AppComponent{
+    currentFocus: string = 'Pinelow Zoo Animal Tacker';
 
+    masterAnimalList: Animal[]= [
+      new Animal('Aligator', 'Bitey', 4, 'Carnivore', 'Reptile Building', 3, 'Female', 'Laying under heatlamp', 'Tapping on the glass'),
+      new Animal('Badger', 'Stripes', 2, 'Omnivore', 'Small Furry Building', 2, 'Male', 'Playing with the red ball', 'Hot weather'),
+      new Animal('Brown Bear', 'Samson', 12 , 'Omnivore', 'Large Beasts', 5, 'Female', 'Frozen Fishcilcles', 'Loud noises')
+    ]
   }
