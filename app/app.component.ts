4706b260809d9f6e54a6235f84dd555d1a791d48
ngottlieb87 <nogpdx@gmail.com>
2017-10-27 08:40:07 -0700

    add initial files and layout

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal.model'
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class= 'container'>
+  </div>
+
+  `
+})
+  export class AppComponent{
+
+  }
---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@ngModule({
+  imports: [ BrowserModule, FormsModule],
+  declarations: [ AppComponent ]
+
+  bootstrap: [ AppComponent ]
+})
+
+
+export class AppModule { }
